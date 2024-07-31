3c3439a18ff0abd48ce87e00f8368f025cc79269
PatrickWorkGithub <patrickgearycarr@gmail.com>
2024-07-31 15:22:02 -0700

    Update MessageRepository.java

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,4 +1,6 @@
 package com.example.repository;
 
-public interface MessageRepository {
+import Lab.Model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface MessageRepository extends JpaRepository<Message, Long> {
 }
